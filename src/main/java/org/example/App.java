c4a292b61386feb3efeee5ef3afbc7781c378571
Simonov Anton <flash_anton@mail.ru>
2023-05-30 13:38:37 +0300

    + swagger-ui

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,15 @@
 package org.example;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
 @RestController
 @RestControllerAdvice
+@Log4j2
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
@@ -26,12 +26,16 @@ public class App {
         return "Hello, World!";
     }
 
+    // e.g. http://localhost:8080/binarySearch?listSize=1_000_000
     @GetMapping(path = "/binarySearch")
-    public ArrayList<Integer> binarySearch(@RequestParam String listSize) {
-        int i = Integer.parseInt(listSize.replaceAll("_",""));
+    public String binarySearch(@RequestParam String listSize) {
+        int i = Integer.parseInt(listSize.replaceAll("_", ""));
         if (i < 1 || i > 1_000_000_000) {
             throw new RuntimeException("listSize must be in range 1 ... 1_000_000_000");
         }
-        return Chapter1.binarySearch(new byte[i], (byte) 1).remains();
+        Chapter1.BinarySearchResult bsr = Chapter1.binarySearch(new byte[i], (byte) 1);
+        String res = String.format("list[%s] required %d steps %s", listSize, bsr.remains().size(), bsr.remains());
+        log.info(res);
+        return res;
     }
 }
